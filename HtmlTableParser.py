148bca626fcc8e70745f4e03d112921e695e94d9
Zar4lli <53489344+Zar4lli@users.noreply.github.com>
2023-07-31 23:37:23 +0300

    Update script
    Change script functions. Update logic

--- HtmlTableParser.py
+++ HtmlTableParser.py
@@ -4,36 +4,30 @@ import time
 import pandas as pd
 
 ## config start
-txtFileName = str(time.time_ns())+'_output.txt' # don't touch!
-excelFileName = str(time.time_ns())+'_output'+'.xlsx' # don't touch!
+txtFileName = str(time.time_ns())+'_output.txt' # Name txt file!
+excelFileName = str(time.time_ns())+'_output'+'.xlsx' # Name exel file!
 
-keysTableHead = ["thead","tr","th"] # Max 3 keys. Needed to looping in tHead table. [items, keys]
-keysTableBody = ["tbody","tr","td"] # Max 3 keys. Needed to looping in tBody table. [items, keys]
+keysTableHead = ["table","thead","tr","td"] # keys to find the head table row
+keysTableBody = ["tr","td"] # keys to find the body table row
 
-MainIdentifyPos = 2 # Main identificator position around 
-MaxCountItemsInRow = 15 # Max items in row in parse table
+MainIdentifyPos = 2 # Main identificator position around  (Started with 1)
+MaxCountItemsInRow = 13 # Max items in row in parse table
 
-urls = ["https://pk-vo.mgpu.ru/pk/dictionary/competitive-group-rating?competitiveGroupId=3067",
-        "https://pk-vo.mgpu.ru/pk/dictionary/competitive-group-rating?competitiveGroupId=3053",
-        "https://pk-vo.mgpu.ru/pk/dictionary/competitive-group-rating?competitiveGroupId=3054",
-        "https://pk-vo.mgpu.ru/pk/dictionary/competitive-group-rating?competitiveGroupId=3048"]
+bNetworkHtml = False
+urls = ["TestTable"]
 
 bSaveHtml = False # Save html's? (False/True)
-namesFileHtml = ["competitiveGroupId3067",
-                 "competitiveGroupId3053",
-                 "competitiveGroupId3054",
-                 "competitiveGroupId3048"] # name's for html files. Only when bSaveHtml = True
 
 bSaveDataInTxtFormat = False # Save out data in txt files? (False/True)
 bSaveDataInExcelFormat = True # Save out data in exel files? (False/True)
-namesSheets = ["Пед образование(СДПП)",
-               "Бизнес-информатика",
-               "Педагогическое образование",
-               "Гос и муниц управление"] # Name of sheet will was smallest 31 character
+namesSheets = ["Sheet_1",
+               "Sheet_2",
+               "Sheet_3",
+               "Sheet_4",
+               "Sheet_4"] # Name of sheet will was smallest 31 character
 
-
-bUseHeaderTable = True # Use auto headerer in a table for StartRow? (False/True)
 bUseCustomStartRow = False # Use custom StartRow (config)? (False/True)
+bUseHeaderTable = False # Use auto headerer in a table for StartRow? (False/True)
 StartRow = [ 
         "№",
         "НОМЕР ЗАЯВЛЕНИЯ",
@@ -50,8 +44,6 @@ StartRow = [
         "ОРИГИНАЛ ДОКУМЕНТА ОБ ОБРАЗОВАНИИ",
         "ПРИОРИТЕТ",
         "ПРОХОДИТЕ ИЛИ НЕ ПРОХОДИТЕ?"] # Start row in all Sheets in excel or text files. Count items in StartRow must be = MaxCountItemsInRow value
-
-cooldown = 0.35 # cooldown 
 
 ## 
 ##
@@ -70,7 +62,10 @@ cooldown = 0.35 # cooldown
 
 
 
-
+cooldown = 0.35 # cooldown 
+namesFileHtml = []
+startData = {}
+data = {}
 
 class RowInfo:
     def __init__(self, idendificate):
@@ -84,45 +79,31 @@ class RowInfo:
     
 def rowTdRecycling(listItemRow, dictionary):
     # check, can MainIdentifyPos out of list range
-    if MainIdentifyPos > len(listItemRow):
+    if (MainIdentifyPos - 1)  > len(listItemRow):
         #print('func rowTdRecycling(): listItemRow {', str(len(listItemRow)), '} identificatePos: {',str(identificatePos), '}')
         return
 
     
-    identify = listItemRow[MainIdentifyPos]
+    identify = listItemRow[MainIdentifyPos-1]
     #add in dictionary. Have dictionary this idendificator?
     if dictionary.get(identify)== None:
         dictionary[identify] = RowInfo(identify) # create new record
 
     pos = 0
     for item in listItemRow:
-        if pos > MaxCountItemsInRow:
+        if pos >= MaxCountItemsInRow:
             break
         oldText = dictionary[identify].data[pos]
         dictionary[identify].data[pos] = dictionary[identify].data[pos].replace(oldText, item)
         pos+=1
-
-
-startData = {}
-data = {}
+    
 def parseHtml(contents,index):
 
-    table = BeautifulSoup(contents,"html.parser").table # BeautifulSoup(contents, 'html')
-    #Parse table Head
-    
+    bs = BeautifulSoup(contents,"html.parser") # BeautifulSoup(contents, 'html')
+
+    # Parse table head
     if bUseHeaderTable == True:
-        for thead in table.find_all(keysTableHead[0]):
-            for row in thead.find_all(keysTableHead[1]):
-                listRow = []
-                
-                for item in row.find_all(keysTableHead[2]):
-                    title = item.text
-                    title = title.strip()
-                    listRow.append(title)
-
-                rowTdRecycling(listRow, startData)
-
-    
+        parseHtmlWithKeys(bs,0,keysTableHead,startData)
 
     # CustomStartRow
     if bUseCustomStartRow == True:
@@ -136,49 +117,54 @@ def parseHtml(contents,index):
             startData[identify].data[pos] = startData[identify].data[pos].replace(oldText, item)
             pos+=1
                 
-    #Parse table Body
-    for tbody in table.find_all(keysTableBody[0]):
-        for row in tbody.find_all(keysTableBody[1]):
-            listRow = []
-                
-            for item in row.find_all(keysTableBody[2]):
-                title = item.text
-                title = title.strip()
-                listRow.append(title)
+    # Parse table body
+    parseHtmlWithKeys(bs,0,keysTableBody,data)
 
-            rowTdRecycling(listRow,data)
+def parseHtmlWithKeys(html, index, keysTable, dictionary):
+    if index == len(keysTable):
+        return None
+    if html.find_all(keysTable[index]) == None:
+        print('Error key for parse: ' + keysTable[index])
+        exit(-1)
+    
+    if index == (len(keysTable)-1): # ['tr','td']
+        listRow = []
+        for item in html.find_all(keysTableBody[len(keysTable)-1]):
+            title = item.text
+                        
+            title = title.strip()
+            listRow.append(title)
+                
+        rowTdRecycling(listRow,data)
             
-    
-    
-def urlDownload(index):
-    ### download page
+    else:
+        pos = 0
+        for htmlChild in html.find_all(keysTable[index]):
+            if index == 1:
+                print(index, pos)
+            parseHtmlWithKeys(htmlChild, index+1, keysTable, dictionary)
+            pos += 1
+
+
+def SiteDownload(index):
+    ### Download page
     r = requests.get(urls[index]) #url - reference
     html = r.text
     ### Save page
     if bSaveHtml == True:
-        f = open(namesFileHtml[index],'w', encoding='utf-8')
+        f = open(namesFileHtml[index]+'.html','w', encoding='utf-8')
         f.write(html)
         f.close()
-    ###
-
-    
-    count = 0
-    #with open(namesFileHtml[index], "r", encoding='utf-8') as f:
-        #contents = f.read()
-    
-    #tempNumber = ''
     return html
 
-        
-
 def printFileInTxt(index):
-    fw = open(namesFileHtml[index]+'.txt', 'w', encoding='utf-8')
-    #write First String
+    fw = open(txtFileName, 'w', encoding='utf-8')
+    # Write head row data
     for startStr in startData:
         fw.write(startData[startStr].output('\t') + '\n')
         break
 
-    #write rows data
+    # Write body Rows data
     for row in data:
         fw.write(data[row].output('\t') + '\n')
         
@@ -201,30 +187,46 @@ dataSheets = []
 def memorySheetsSave(index):
 
     listExel = converterToExelData()
-    df = pd.DataFrame(listExel, columns=StartRow) # index=['one', 'two', 'three']
+    if bUseCustomStartRow == True:
+        df = pd.DataFrame(listExel, columns=StartRow) # index=['one', 'two', 'three']
+    elif bUseHeaderTable == True:
+        df = pd.DataFrame(listExel, columns=startData[namesFileHtml[index]])
+    else:
+        df = pd.DataFrame(listExel)
 
     dataSheets.append(df)
-    #df.to_excel( 'pandas_to_excel_no_index_header.xlsx' , index= False , header= False )
-
-##    printInFile(data, str(time.time_ns())+'output', napravlenii,False)
-##    printInFile(data, str(time.time_ns())+'output', napravlenii,True)
 
 
 def writeSheetsToExcel():
     writer = pd.ExcelWriter(excelFileName, engine='xlsxwriter')
     index = 0
     for sheet in dataSheets:
-        sheet.to_excel(writer, sheet_name=namesSheets[index],index=False)
+        if (bUseCustomStartRow == True) and (bUseHeaderTable == True):
+            sheet.to_excel(writer, sheet_name=namesSheets[index],index=False)
+        else:
+            sheet.to_excel(writer, sheet_name=namesSheets[index],index=False, columns=None)
         index += 1
     writer._save()
 
 
 
 def parseMain():
+    if len(namesFileHtml) == 0:
+        for pos in range(len(urls)):
+            namesFileHtml.append('site'+str(pos+1))
+            
     for index in range(len(urls)):
+        html = ''
         
-        html = urlDownload(index)
-        print("[Downloaded] ",urls[index][35:])
+        if bNetworkHtml == True:
+            html = SiteDownload(index)
+            print("[Downloaded] ",urls[index][35:])
+        else:
+            fileHtml = open(urls[index]+'.html','r',encoding='utf-8')
+            html = fileHtml.read()
+            print("[Open File Html] Open: ",urls[index])
+            fileHtml.close()
+            
         parseHtml(html,index)
         
         if bSaveDataInTxtFormat == True:
@@ -233,10 +235,10 @@ def parseMain():
         
         if bSaveDataInExcelFormat==True:
             memorySheetsSave(index)
-            print("[Pass]\t\tSheet table saved in memory")
+            print("[Pass]\tSheet table saved in memory")
 
-        startData.clear() #
-        data.clear() #
+        startData.clear() 
+        data.clear() 
         
         time.sleep(cooldown)
         
